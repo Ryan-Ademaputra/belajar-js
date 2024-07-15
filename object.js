e2b562a3b503c40d69e51c21ae06228d7cb9c65d
Ryan-Ademaputra <ademaputraryan@gmail.com>
2024-07-15 21:19:36 +0700

    menambahkan file type object

---
+++ object.js
@@ -0,0 +1,13 @@
+const comunity = "Bearmentor";
+
+let user = {
+    nama: "Ryan Ademaputra",
+    asal: "Bandung",
+    instagram: "ry4nn.x",
+    github: "https://github.com/Ryan-Ademaputra",
+
+    "Blog":
+        `Saya sedang mengikuti Bootcamp yaitu di${comunity} semoga saya selalu konsisten`
+};
+
+console.log(user)
