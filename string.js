5ed9046f814165aaa5d6c78d6fc649d20c4f715f
Ryan-Ademaputra <ademaputraryan@gmail.com>
2024-07-15 20:32:52 +0700

    menambahkan beberapa kode dan console,log

---
+++ string.js
@@ -0,0 +1,17 @@
+let nama = " Ryan Ademaputra";
+let game = "mobile legens";
+let umur = 18;
+
+
+
+
+console.log(`Hallo Nama saya${nama} hobi saya adalah bermain game ${game} sekarang umur saya adalah ${umur}`);
+console.log(`kemarin saya pergi bersama${nama} ke kebun binatang disana saya malah diajak main game${game}`);
+console.log(`hasil bilangan dari 1 + 2 adalah ${1 + 2}.`);
+console.log(`
+    #note
+    
+    - kedepannya harus lebih konsisten dalam belajar programmer
+    - rajin solat
+    
+    `)
