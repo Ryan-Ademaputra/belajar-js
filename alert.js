6ac97e4f6bca2558e5fc7bcbaefe9f73759315df
Ryan-Ademaputra <ademaputraryan@gmail.com>
2024-08-03 11:36:16 +0700

    add new file

---
+++ alert.js
@@ -0,0 +1,5 @@
+const fungsi = apa(title, [deafult]);
+
+const name = window.apa("whats your name");
+
+console.log(name)
---
+++ array_object.js
@@ -0,0 +1,22 @@
+let user = [
+    {
+        id: 1,
+        nama: "Ryan",
+        favSong: ["i wanna be yours", "scoot street"],
+        makanan: "apa aja",
+    },
+    {
+        id: 2,
+        nama: "Udin",
+        favSong: ["xxx xxx xxx", "xxx xxx", "apa aja"],
+        makanan: "apa weh",
+    },
+    {
+        id: 3,
+        nama: "asep",
+        favSong: ["xxx xxx xxx", "xx xx xx", ""],
+        makanan: "kepo"
+    }
+];
+
+console.log(user);
